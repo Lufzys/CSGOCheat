be02844ab956c0f28b1dc445c00445d2b311753c
Furkan "Lufzys <53129220+Lufzys@users.noreply.github.com>
2022-02-18 01:48:33 +0300

    Added : Source :100:

---
+++ DXOverlay/DXOverlay/Classes/SDK/Managment/EngineClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static Memory.Memorys;
+
+namespace DXOverlay.Classes.SDK.Managment
+{
+    class EngineClient
+    {
+        public static int ClientState => Read<int>(Game.Engine + Game.Offsets.signatures.dwClientState);
+        public static Variables.Enums.SignOnState SignOnState => (Variables.Enums.SignOnState)Read<int>(ClientState + Game.Offsets.signatures.dwClientState_State);
+        public static bool IsInGame => SignOnState == Variables.Enums.SignOnState.FULL;
+        public static int MaxPlayer => Read<int>(ClientState + Game.Offsets.signatures.dwClientState_MaxPlayer);
+        public static void ForceUpdate() => Write<int>(ClientState + Game.Offsets.signatures.clientstate_delta_ticks, -1);
+        public static float[] ViewMatrix => ReadMatrix<float>(Game.Client + Game.Offsets.signatures.dwViewMatrix, 16);
+        public static Vector3 ViewAngles
+        {
+            get
+            {
+                return Read<Vector3>(ClientState + Game.Offsets.signatures.dwClientState_ViewAngles);
+            }
+            set
+            {
+                Write<Vector3>(ClientState + Game.Offsets.signatures.dwClientState_ViewAngles, value);
+            }
+        }
+    }
+}
---
+++ DXOverlay/DXOverlay/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DXOverlay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "DXOverlay - Example Cheat";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" \n  DXOverlay - Example Cheat for CSGO");
+            if(System.Diagnostics.Process.GetProcessesByName("csgo").Length != 0)
+            {
+                Console.WriteLine("  Menu Key - Insert");
+                Overlay overlay = new Overlay();
+                overlay.Init("csgo");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("  Error : CSGO not found!");
+                Thread.Sleep(2500);
+            }
+        }
+    }
+}
